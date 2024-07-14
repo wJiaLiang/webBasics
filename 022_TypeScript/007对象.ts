a9939677c1536b3cb1266e1452877f8035bc3c3e
kg <kgwei520@163.com>
2024-07-14 22:29:18 +0800

    fix:...

--- 022_TypeScript/007对象.ts
+++ 022_TypeScript/007对象.ts
@@ -113,8 +113,170 @@ const obj:MyObj = {
 
 */ 
 
+/*
+五、结构赋值
+解构赋值用于直接从对象中提取属性。
+const {id, name, price} = product;
+从对象product提取了三个属性，并声明属性名的同名变量。
+
+解构赋值的类型写法，跟为对象声明类型是一样的。
+const {id, name, price}:{
+  id: string;
+  name: string;
+  price: number
+} = product;
+
+注意，目前没法为解构变量指定类型，因为对象解构里面的冒号，JavaScript 指定了其他用途。
+let { x: foo, y: bar } = obj;
+
+// 等同于
+let foo = obj.x;
+let bar = obj.y;
+冒号不是表示属性x和y的类型，而是为这两个属性指定新的变量名。如果要为x和y指定类型，不得不写成下面这样。
+let { x: foo, y: bar }:{ x: string; y: number } = obj;
+
+这一点要特别小心，TypeScript 里面很容易搞糊涂。
+
+function draw({
+  shape: Shape,
+  xPos: number = 100
+}) {
+  let myShape = shape; // 报错
+  let x = xPos; // 报错
+}
+
+数draw()的参数是一个对象解构，里面的冒号很像是为变量指定类型，其实是为对应的属性指定新的变量名。
+所以，TypeScript 就会解读成，函数体内不存在变量shape，而是属性shape的值被赋值给了变量Shape。
+
+*/ 
+
+/*
+六、结构类型原则
+只要对象 B 满足 对象 A 的结构特征，TypeScript 就认为对象 B 兼容对象 A 的类型，这称为“结构类型”原则（structural typing）。
+type A = {
+  x: number;
+};
+
+type B = {
+  x: number;
+  y: number;
+};
+对象A只有一个属性x，类型为number。对象B满足这个特征，因此兼容对象A，只要可以使用A的地方，就可以使用B
+
+
+*/
+
+/*
+七、严格字面量检查
+如果对象使用字面量表示，会触发 TypeScript 的严格字面量检查（strict object literal checking）。
+如果字面量的结构跟类型定义的不一样（比如多出了未定义的属性），就会报错。
+const point:{
+  x:number;
+  y:number;
+} = {
+  x: 1,
+  y: 1,
+  z: 1 // 报错
+};
+如果等号右边不是字面量，而是一个变量，根据结构类型原则，是不会报错的。
+const myPoint = {
+  x: 1,
+  y: 1,
+  z: 1
+};
+
+const point:{
+  x:number;
+  y:number;
+} = myPoint; // 正确
+
+如果允许字面量有多余属性，可以像下面这样在类型里面定义一个通用属性。
+let x: {
+  foo: number,
+  [x: string]: any
+};
+
+x = { foo: 1, baz: 2 };  // Ok
+上面示例中，变量x的类型声明里面，有一个属性的字符串索引（[x: string]），导致任何字符串属性名都是合法的。
+
+
+
+*/ 
+/*
+八、最小可选属性规则
+根据“结构类型”原则，如果一个对象的所有属性都是可选的，那么其他对象跟它都是结构类似的。
+类型Options的所有属性都是可选的，所以它可以是一个空对象，也就意味着任意对象都满足Options的结构。
+对象opts与类型Options没有共同属性，赋值给该类型的变量就会报错。
+
+如果某个类型的所有属性都是可选的，那么该类型的对象必须至少存在一个可选属性，
+不能所有可选属性都不存在。这就叫做“最小可选属性规则”。
+
+type Options = {
+  a?:number;
+  b?:number;
+  c?:number;
+};
+
+const opts = { d: 123 };
+
+const obj:Options = opts; // 报错
+要么在类型里面增加一条索引属性（[propName: string]: someType），要么使用类型断言（opts as Options）。
+
+*/ 
+
+
+type Options = {
+  a?:number;
+  b?:number;
+  c?:number;
+  [xs:string]:any
+};
+
+const opts = { d: 123 };
+
+const obj:Options = opts; // 报错
+
+
+/*
+九、空对象
+空对象是 TypeScript 的一种特殊值，也是一种特殊类型。
+
+const obj = {};
+obj.prop = 123; // 报错
+上面示例中，变量obj的值是一个空对象，然后对obj.prop赋值就会报错。
+原因是这时 TypeScript 会推断变量obj的类型为空对象，实际执行的是下面的代码。
+const obj:{} = {};
+
+空对象没有自定义属性，所以对自定义属性赋值就会报错。空对象只能使用继承的属性，即继承自原型对象Object.prototype的属性。
+obj.toString() // 正确
+
+这种写法其实在 JavaScript 很常见：先声明一个空对象，然后向空对象添加属性。
+但是，TypeScript 不允许动态添加属性，所以对象不能分步生成，必须生成时一次性声明所有属性。
+
+如果确实需要分步声明，一个比较好的方法是，使用扩展运算符（...）合成一个新对象。
+const pt0 = {};
+const pt1 = { x: 3 };
+const pt2 = { y: 4 };
+
+const pt = {
+  ...pt0, ...pt1, ...pt2
+};
+
+
+空对象作为类型，其实是Object类型的简写形式。
+
+let d:{};
+// 等同于
+// let d:Object;
+
+d = {};
+d = { x: 1 };
+d = 'hello';
+d = 2;
+各种类型的值（除了null和undefined）都可以赋值给空对象类型，跟Object类型的行为是一样的。
 
 
 
 
 
+*/ 
---
+++ 022_TypeScript/008interface.ts
@@ -0,0 +1,369 @@
+/*
+
+一、简介
+interface 是对象的模板，可以看作是一种类型约定，中文译为“接口”。使用了某个模板的对象，就拥有了指定的类型结构。
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+定义了一个接口Person，它指定一个对象模板，拥有三个属性firstName、lastName和age。
+任何实现这个接口的对象，都必须部署这三个属性，并且必须符合规定的类型。
+
+实现该接口很简单，只要指定它作为对象的类型即可。
+const p:Person = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 25
+};
+变量p的类型就是接口Person，所以必须符合Person指定的结构。
+方括号运算符可以取出 interface 某个属性的类型。
+
+interface Foo {
+  a: string;
+}
+type A = Foo['a']; // string
+Foo['a']返回属性a的类型，所以类型A就是string。
+
+interface 可以表示对象的各种语法，它的成员有5种形式。
+对象属性
+对象的属性索引
+对象方法
+函数
+构造函数
+
+
+（1）对象属性
+interface Point {
+  x: number;
+  y: number;
+}
+上面示例中，x和y都是对象的属性，分别使用冒号指定每个属性的类型。
+属性之间使用分号或逗号分隔，最后一个属性结尾的分号或逗号可以省略。
+如果属性是可选的，就在属性名后面加一个问号。
+如果属性是只读的，需要加上readonly修饰符。
+interface Foo {
+  x?: string;
+  readonly a: string;
+
+}
+
+（2）对象的属性索引
+interface A {
+  [prop: string]: number;
+}
+上面示例中，[prop: string]就是属性的字符串索引，表示属性名只要是字符串，都符合类型要求。
+属性索引共有string、number和symbol三种类型。
+一个接口中，最多只能定义一个字符串索引。字符串索引会约束该类型中所有名字为字符串的属性。
+interface MyObj {
+  [prop: string]: number;
+
+  a: boolean;      // 编译错误
+}
+属性索引指定所有名称为字符串的属性，它们的属性值必须是数值（number）。属性a的值为布尔值就报错了。
+
+属性的数值索引，其实是指定数组的类型。
+
+
+一个接口中最多只能定义一个数值索引。数值索引会约束所有名称为数值的属性。
+如果一个 interface 同时定义了字符串索引和数值索引，那么数值索引必须服从于字符串索引。
+因为在 JavaScript 中，数值属性名最终是自动转换成字符串属性名。
+
+interface B {
+  [prop: string]: number;
+  [prop: number]: number; // 正确
+}
+
+
+（3）对象的方法
+对象的方法共有三种写法。
+// 写法一
+interface A {
+  f(x: boolean): string;
+}
+// 写法二
+interface B {
+  f: (x: boolean) => string;
+}
+// 写法三
+interface C {
+  f: { (x: boolean): string };
+}
+// 写法四、属性名可以采用表达式，所以下面的写法也是可以的。
+const f = 'f';
+interface A {
+  [f](x: boolean): string;
+}
+
+类型方法可以重载。
+interface A {
+  f(): number;
+  f(x: boolean): boolean;
+  f(x: string, y: string): string;
+}
+
+（4）函数
+interface 也可以用来声明独立的函数。
+interface Add {
+  (x:number, y:number): number;
+}
+const myAdd:Add = (x,y) => x + y;
+
+（5）构造函数
+interface 内部可以使用new关键字，表示构造函数。
+interface ErrorConstructor {
+  new (message?: string): Error;
+}
+
+
+
+
+*/ 
+
+
+/*
+二、interface 的继承
+interface 可以继承其他类型，主要有下面几种情况。
+
+1、interface 继承 interface # 
+interface 可以使用extends关键字，继承其他 interface。
+interface Shape {
+  name: string;
+}
+
+interface Circle extends Shape {
+  radius: number;
+}
+Circle继承了Shape，所以Circle其实有两个属性name和radius。这时，Circle是子接口，Shape是父接口。
+extends关键字会从继承的接口里面拷贝属性类型，这样就不必书写重复的属性。
+interface 允许多重继承。
+
+interface Style {
+  color: string;
+}
+interface Shape {
+  name: string;
+}
+interface Circle extends Style, Shape {
+  radius: number;
+}
+extends关键字会从继承的接口里面拷贝属性类型，这样就不必书写重复的属性。
+interface Style {
+  color: string;
+}
+
+interface Shape {
+  name: string;
+}
+
+interface Circle extends Style, Shape {
+  radius: number;
+}
+Circle同时继承了Style和Shape，所以拥有三个属性color、name和radius。
+多重接口继承，实际上相当于多个父接口的合并。
+
+如果子接口与父接口存在同名属性，那么子接口的属性会覆盖父接口的属性。
+注意，子接口与父接口的同名属性必须是类型兼容的，不能有冲突，否则会报错。
+
+Bar继承了Foo，但是两者的同名属性id的类型不兼容，导致报错。
+多重继承时，如果多个父接口存在同名属性，那么这些同名属性不能有类型冲突，否则会报错。
+
+
+
+2、Bar继承了Foo，但是两者的同名属性id的类型不兼容，导致报错。
+多重继承时，如果多个父接口存在同名属性，那么这些同名属性不能有类型冲突，否则会报错。
+
+interface 可以继承type命令定义的对象类型。
+type Country = {
+  name: string;
+  capital: string;
+}
+
+interface CountryWithPop extends Country {
+  population: number;
+}
+CountryWithPop继承了type命令定义的Country对象，并且新增了一个population属性。
+注意，如果type命令定义的类型不是对象，interface 就无法继承。
+
+
+3、interface 继承 class 
+interface 还可以继承 class，即继承该类的所有成员。
+class A {
+  x:string = '';
+
+  y():boolean {
+    return true;
+  }
+}
+
+interface B extends A {
+  z: number
+}
+
+
+
+
+*/ 
+
+
+/*
+三、接口合并
+
+interface Box {
+    height: number;
+    width: number;
+  }
+  
+  interface Box {
+    length: number;
+  }
+
+上面示例中，两个Box接口会合并成一个接口，同时有height、width和length三个属性。
+这样的设计主要是为了兼容 JavaScript 的行为。JavaScript 开发者常常对全局对象或者外部库，添加自己的属性和方法。
+那么，只要使用 interface 给出这些自定义属性和方法的类型，就能自动跟原始的 interface 合并，使得扩展外部类型非常方便。
+
+
+Web 网页开发经常会对window对象和document对象添加自定义属性，但是 TypeScript 会报错，因为原始定义没有这些属性。
+解决方法就是把自定义属性写成 interface，合并进原始定义。
+interface Document {
+  foo: string;
+}
+document.foo = 'hello';
+上面示例中，接口Document增加了一个自定义属性foo，从而就可以在document对象上使用自定义属性。
+
+同名接口合并时，同一个属性如果有多个类型声明，彼此不能有类型冲突。
+interface A {
+  a: number;
+}
+
+interface A {
+  a: string; // 报错
+}
+如果两个 interface 组成的联合类型存在同名属性，那么该属性的类型也是联合类型。
+interface Circle {
+  area: bigint;
+}
+interface Rectangle {
+  area: number;
+}
+
+declare const s: Circle | Rectangle;
+s.area;   // bigint | number
+
+
+
+
+*/ 
+
+
+/*
+四、interface 与 type 的异同
+interface命令与type命令作用类似，都可以表示对象类型。
+很多对象类型既可以用 interface 表示，也可以用 type 表示。而且，两者往往可以换用，
+几乎所有的 interface 命令都可以改写为 type 命令。
+
+它们的相似之处，首先表现在都能为对象类型起名。
+type Country = {
+  name: string;
+  capital: string;
+}
+
+interface Country {
+  name: string;
+  capital: string;
+}
+type命令和interface命令，分别定义同一个类型。
+class命令也有类似作用，通过定义一个类，同时定义一个对象类型。但是，它会创造一个值，编译后依然存在。
+如果只是单纯想要一个类型，应该使用type或interface。
+
+interface 与 type 的区别有下面几点。
+（1）type能够表示非对象类型，而interface只能表示对象类型（包括数组、函数等）。
+（2）interface可以继承其他类型，type不支持继承。
+继承的主要作用是添加属性，type定义的对象类型如果想要添加属性，只能使用&运算符，重新定义一个类型。
+
+type Animal = {
+  name: string
+}
+
+type Bear = Animal & {
+  honey: boolean
+}
+上面示例中，类型Bear在Animal的基础上添加了一个属性honey。
+上例的&运算符，表示同时具备两个类型的特征，因此可以起到两个对象类型合并的作用。
+作为比较，interface添加属性，采用的是继承的写法。
+interface Animal {
+  name: string
+}
+interface Bear extends Animal {
+  honey: boolean
+}
+继承时，type 和 interface 是可以换用的。interface 可以继承 type。
+type Foo = { x: number; };
+interface Bar extends Foo {
+  y: number;
+}
+
+type 也可以继承 interface。
+interface Foo {
+  x: number;
+}
+
+type Bar = Foo & { y: number; };
+
+（3）同名interface会自动合并，同名type则会报错。也就是说，TypeScript 不允许使用type多次定义同一个类型。
+type A = { foo:number }; // 报错
+type A = { bar:number }; // 报错
+上面示例中，type两次定义了类型A，导致两行都会报错。
+
+（4）interface不能包含属性映射（mapping），type可以，详见《映射》一章。
+interface Point {
+  x: number;
+  y: number;
+}
+
+// 正确
+type PointCopy1 = {
+  [Key in keyof Point]: Point[Key];
+};
+// 报错
+interface PointCopy2 {
+  [Key in keyof Point]: Point[Key];
+};
+
+（5）this关键字只能用于interface。
+// 正确
+interface Foo {
+  add(num:number): this;
+};
+// 报错
+type Foo = {
+  add(num:number): this;
+};
+
+（6）type 可以扩展原始数据类型，interface 不行。
+// 正确
+type MyStr = string & {
+  type: 'new'
+};
+
+// 报错
+interface MyStr extends string {
+  type: 'new'
+}
+
+（7）interface无法表达某些复杂类型（比如交叉类型和联合类型），但是type可以。
+type A = { };
+type B = { };
+
+type AorB = A | B;
+type AorBwithName = AorB & {
+  name: string
+};
+
+
+
+
+
+
+*/ 
