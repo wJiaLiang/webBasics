1c943c076709bef85398fac28e8d8abba41649a8
kg <kgwei520@163.com>
2025-01-07 22:51:36 +0800

    配置

--- 024_nuxt3/nuxt.config.ts
+++ 024_nuxt3/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // compatibilityDate 是 Nuxt 的一个配置选项,用于指定 Nuxt 版本的兼容性日期
+  // 它可以帮助我们在升级 Nuxt 版本时保持向后兼容性
+  // 当设置为 '2024-11-01' 时,表示使用该日期对应的 Nuxt 功能特性
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
@@ -7,6 +10,7 @@ export default defineNuxtConfig({
   css:[
     "~/assets/css/base.css",
   ],
+  //引入 ant ui;
   modules: [
     '@ant-design-vue/nuxt'
   ],
@@ -16,7 +20,25 @@ export default defineNuxtConfig({
 
   runtimeConfig:{
     isServer:true,
-  }
+    public:{
+      // baseURL 用于配置 API 请求的基础路径
+      // 在 runtimeConfig.public 中定义的配置可以在客户端和服务端都能访问
+      // 如果设置为相对路径 '/api'，请求会基于当前域名拼接，如: http://localhost:3000/api/xxx
+      // 如果设置为绝对路径 'http://example.com/api'，则直接使用完整的URL进行请求
+      // 绝对路径通常用于跨域请求或指定特定的API服务器
+      baseURL:'http://example.com/api',
+    }
+  },
+  app:{
+    baseURL:'/web-pc',
+    head:{
+      title:"后台管理系统",
+      meta:[
+        {name:"viewport",content:"width=device-width, initial-scale=1"}
+      ]
+    }
+  },
+  
 
   
 })
