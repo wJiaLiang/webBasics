a09c1dfddd730de62cda727283b4699007e7b958
wJiaLiang <20158614+wJiaLiang@users.noreply.github.com>
2025-11-18 15:09:48 +0800

    fix:地图

--- 021_vite_vue3/src/views/home/index.vue
+++ 021_vite_vue3/src/views/home/index.vue
@@ -4,14 +4,16 @@
         <div class="tc">home 页面</div>
     </div>
     <div class="pt20 pb20 pl20 pr20" style="border:1px solid burlywood;">
-      <router-link class="pl10 pr10" to="/dialog">弹框</router-link>
-      <router-link class="pl10 pr10" to="/deepseek"> deepseek </router-link>
-      <router-link class="pl10 pr10" to="/pinia"> pinia </router-link>
-      <router-link class="pl10 pr10" to="/composable"> composable组合式API </router-link>
+      <router-link class="pl10 pr10 dis_b" to="/dialog">弹框</router-link>
+      <router-link class="pl10 pr10 dis_b" to="/deepseek"> deepseek </router-link>
+      <router-link class="pl10 pr10 dis_b" to="/pinia"> pinia </router-link>
+      <router-link class="pl10 pr10 dis_b" to="/composable"> composable组合式API </router-link>
+      <div class="pl10 pt10 dis_b">
+        <a href="https://uri.amap.com/marker?position=104.069356,30.578654">打开高德定位到成都</a>
+      </div>
     </div>
 
 
-    
     <Tabbar :active="0"></Tabbar>
 
   </div>
